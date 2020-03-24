52feaa0947d2b8459cc051599a61af52b465efb0
ajayk2233 <ajay.watchout@gmail.com>
2020-03-24 13:57:53 -0700

    heroku configuration

--- Todo_list/settings.py
+++ Todo_list/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -143,3 +143,6 @@ else:
     EMAIL_HOST_USER = 'ajayimp2@gmail.com'
     EMAIL_HOST_PASSWORD = '@AJAY@ADITI@'
     DEFAULT_FROM_EMAIL = 'My-Todo Team <noreply@ajaykhajindar.com'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
